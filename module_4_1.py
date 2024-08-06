b12ccec6782f33e3527290dfa3823faa7d6a15dd
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-08-06 22:35:48 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+# Домашняя работа по уроку "Модули и пакеты".
+
+
+from Fake_math import divide as fake_divide
+from True_math import divide as true_divide
+print('resultat_1 = ', fake_divide(69, 3))
+print('resultat_2 = ', fake_divide(3, 0))
+print('resultat_3 = ', true_divide(49, 7))
+print('resultat_4 = ', true_divide(15, 0))
+
+
+
+
